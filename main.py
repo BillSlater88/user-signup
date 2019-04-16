565bbb51fac48fd2a7c77f183cbe0ed655d30656
Bill Slater <william.slater527@gmail.com>
2019-04-15 19:23:54 -0500

    Done with User-signup

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, render_template
 
 import cgi
+
+import re
+import os
+import jinja2
+
+
 
 app = Flask(__name__)
 
@@ -22,24 +28,76 @@ page_footer = """
     </body>
 </html>
 """
+ 
+
+def proper_length(entry):
+    
+    if len(entry) > 2 and len(entry) < 21:
+        return True
+
+def proper_email(entry):
+    if 2 < len(entry) < 21 and re.match(r"^[A-Za-z0-9\.\+_-]+@[A-Za-z0-9\._-]+\.[a-zA-Z]*$", entry) != None:
+        return True
+
+def has_spaces(entry):
+    if ' ' in entry:
+        return True
+
+@app.route('/', methods=["GET"])
+def display_form():
+    return render_template('table.html', username='', username_error='', password='', password_error='',
+     password_check='', password_check_error='', email='', email_error='')
 
 
-username_form = """
-    <form action="/username" method="post">
-        <label>
-            Username
-            <input type="text" name="username"/>
-        </label>
-        <input type="submit" value="Submit Query"/>
-    </form>
-"""
+@app.route('/', methods=["POST"])
+def validate_information():
+
+    username = request.form['username']
+    username_error = ''
+
+    password = request.form['password']
+    password_error = ''
+
+    password_check = request.form['verifypassword']
+    password_check_error = ''
+
+    email = request.form['email']
+    email_error = ''
+
+    if has_spaces(username):
+        username_error = 'Your username cannot contain spaces.'
+
+    if has_spaces(password):
+        password_error = 'Your password cannot contain spaces.'
+        password = ''
 
 
-@app.route("/")
-def index():
+    if  not proper_length(username):
+        username_error = 'Your username must be between 3 and 20 characters.'
+        #username = ''
 
-    return page_header + username_form + page_footer
+    if not proper_length(password):
+        password_error = 'Your password must be between 3 and 20 characters.'
+        password = ''
 
+    
+    if password != password_check:
+        password_check_error = 'Your passwords do not match.'
+        password_check = ''
+        password = ''
+    
+    if not proper_email(email) and email != '':
+        email_error = 'Your email is not in the correct format or is not between 3 and 20 characters.'
+
+    if not username_error and not password_error and not password_check_error and not email_error:
+        name = request.form['username']
+        return "Welcome {0}!".format(name)
+    else:
+        return render_template('table.html', username_error=username_error, password_error=password_error,
+        password_check_error=password_check_error, email_error=email_error, username=username, 
+        password=password, password_check=password_check, email=email)
+        
+        
 
 
 app.run()
